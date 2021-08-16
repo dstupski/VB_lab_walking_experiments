c65ec409ebc029fae6c6b5e62a84508c386f2934
dstupski <81446311+dstupski@users.noreply.github.com>
2021-08-16 16:04:47 -0700

    Delete demo_2 directory

--- demo_2/src/delta_video_config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    def __init__(self):
-        self.basename = 'delta_video'
-        self.directory = '~/demo/demo_2/data'
-        self.topics = ['/multi_tracker/2/delta_video',]
--- demo_2/src/raw_data_bag_config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    def __init__(self):
-        self.basename = 'raw_data_N2'
-        self.directory = '~/demo/demo_2/data'
-        self.topics = ['/multi_tracker/2/tracked_objects']
