dc64179d24d20b576aa35b48c158511c38e80621
dstupski <81446311+dstupski@users.noreply.github.com>
2021-08-16 15:03:06 -0700

    Add files via upload

---
+++ demo_test/src/delta_video_config.py
@@ -0,0 +1,6 @@
+class Config:
+    def __init__(self):
+        self.basename = 'delta_video'
+        self.directory = '~/demo/demo_1/data'
+        self.topics = ['/multi_tracker/1/delta_video',]
+        
